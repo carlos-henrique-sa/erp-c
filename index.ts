773f85d5c01e0dc2a554671bcc088a6f8f84cd50
carlos-henrique-sa <carlos.henrique.sa.filho@gmail.com>
2025-05-16 06:13:38 -0300

    updating files

---
+++ index.ts
@@ -0,0 +1,26 @@
+import { PrismaClient } from './generated/prisma';
+
+const prisma = new PrismaClient();
+
+async function main() {
+  // ... you will write your Prisma Client queries here
+  await prisma.user.create({
+    data: {
+      email: 'alice@prisma.io',
+      password: 'teste',
+    },
+  });
+
+  const allUsers = await prisma.user.findMany();
+  console.dir(allUsers, { depth: null });
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
--- src/app.controller.ts
+++ src/app.controller.ts
@@ -9,4 +9,9 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Get('/something')
+  getSomethingElse(): string {
+    return 'teste';
+  }
 }
